46f22bcaeaf676bece5c3f563e9ac83966a20854
SPIDER <162958422+spidervsyou@users.noreply.github.com>
2024-05-09 21:50:07 +0300

    Update Pet.js

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,27 +1,36 @@
-import React from "react";
+import React from 'react';
 
-function Pet() {
+const Pet = ({ pet, onAdoptPet }) => {
+  const handleAdoptClick = () => {
+    onAdoptPet(pet.id);
+  };
+
   return (
-    <div className="card" data-testid="pet">
+    <div className="card">
       <div className="content">
-        <span className="header">
-          {/*'♀' OR '♂' */}
-          PET NAME
-        </span>
+        <div className="header">
+          {pet.gender === 'male' ? '♂ ' : '♀ '}
+          {pet.name}
+        </div>
         <div className="meta">
-          <span className="date">PET TYPE</span>
+          <span className="date">{pet.type}</span>
         </div>
         <div className="description">
-          <p>Age: PET AGE</p>
-          <p>Weight: PET WEIGHT</p>
+          <p>Age: {pet.age}</p>
+          <p>Weight: {pet.weight}</p>
         </div>
       </div>
       <div className="extra content">
-        <button className="ui disabled button">Already adopted</button>
-        <button className="ui primary button">Adopt pet</button>
+        {pet.isAdopted ? (
+          <button className="ui disabled button">Already adopted</button>
+        ) : (
+          <button className="ui primary button" onClick={handleAdoptClick}>
+            Adopt pet
+          </button>
+        )}
       </div>
     </div>
   );
-}
+};
 
 export default Pet;
